aa97ad088203620fd2604e203e3d8be73ffdbf3d
Wasif Zaman <teatocode@gmail.com>
2017-07-11 17:37:55 -0400

    Attempted find, unfinished

--- commands/dirCommands.js
+++ commands/dirCommands.js
@@ -11,15 +11,32 @@ function ls(stdin, path, done) {
     if (err) throw err;
     var output = "";
     files.forEach(function(file) {
-      output += file.toString() + "\t";
+      output += file.toString() + "\n";
     })
     done(output.trim(), stdin);
   });
 }
 
-function find(stdin, args, done) {
-  //incomplete
-  done("", stdin);
+function find(stdin, path, done) {
+  // run find on the first layer
+  // if the length of find is > 1 run find again
+  var output = [], i = 0;
+
+  function readAll(filePath) {
+    fs.readdir(filePath = filePath[0] || '.', function(err, files) {
+      output[i++] = filePath;
+      if (files) {
+        files.forEach(function(item, index) {
+          output[i++] = item;
+          readAll([item]);
+          // if (index + 1 == files.length) done(output.join('\n'), stdin);
+        })
+      }
+      done(output.join('\n'), stdin)
+    })
+  };
+
+  readAll(path);
 }
 
 module.exports = {
